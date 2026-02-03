89a9ed8e33164c4d087aba6def6145ceba168f81
Hen-Heang <henheang15@gmail.com>
2026-02-03 17:50:37 +0900

    test mapper

---
+++ src/main/java/com/heang/springmybatistest/controller/DbTestController.java
@@ -0,0 +1,20 @@
+package com.heang.springmybatistest.controller;
+
+import com.heang.springmybatistest.mapper.TestMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DbTestController {
+
+    private final TestMapper testMapper;
+
+    public DbTestController(TestMapper testMapper) {
+        this.testMapper = testMapper;
+    }
+
+    @GetMapping("/db-test")
+    public String dbTest() {
+        return testMapper.now();
+    }
+}
---
+++ src/main/java/com/heang/springmybatistest/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.heang.springmybatistest.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface TestMapper {
+
+    @Select("select now()")
+    String now();
+}
